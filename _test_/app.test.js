28ba8e3263197017cabb98c479f297c3f3948cd3
Joseph-K-B <303josephb@gmail.com>
2021-09-20 16:53:39 -0700

    test still failing recieving 404

--- _test_/app.test.js
+++ _test_/app.test.js
@@ -1,11 +1,11 @@
 import request from 'supertest';
 import app from '../lib/app.js';
-import index from '../public/index.html';
+// const index = '../public/index.html';
 
 describe('http server app routes', () => {
   it('gets index.html file from get /index', async () => {
-    const res = await request(app).get('/index');
+    const res = await request(app).get('/');
     expect (res.statusCode).toEqual(200);
-    expect(res.text).toEqual(index);
+    // expect(res.text).toEqual(index);
   });
 });
