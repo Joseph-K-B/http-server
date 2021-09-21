513defc3c1744c1255ff8420a927dbe12c2b9f2d
Joseph-K-B <303josephb@gmail.com>
2021-09-20 17:43:01 -0700

    returning 200 and all index.html, improper formating, still failing

--- _test_/app.test.js
+++ _test_/app.test.js
@@ -1,11 +1,23 @@
 import request from 'supertest';
 import app from '../lib/app.js';
-// const index = '../public/index.html';
+const index = '../public/index.html';
 
 describe('http server app routes', () => {
   it('gets index.html file from get /index', async () => {
     const res = await request(app).get('/');
     expect (res.statusCode).toEqual(200);
-    // expect(res.text).toEqual(index);
+    expect(res.text).toEqual(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='./css/main.css'>
+    <title>Document</title>
+</head>
+<body>
+    <h1>Text</h1>
+</body>
+</html>`);
   });
 });
